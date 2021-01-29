34e1772a6905995c1f9050b48d804b68d90566dc
BwanaQ <thunjawax@gmail.com>
2021-01-29 08:31:08 +0300

    deploy to gh-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'nirvana';
 }
+/*
+bg color 1c1c1c
+text ffffff
+price border 494949
+button 800000
+btn text ffffff
+datepicker 1c1c1c
+*/
