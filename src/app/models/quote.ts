277c471091de7ac69f0736dc820118af33abed44
BwanaQ <thunjawax@gmail.com>
2021-01-29 15:58:08 +0300

    add static quotes to quote array

--- src/app/models/quote.ts
+++ src/app/models/quote.ts
@@ -1,10 +1,13 @@
 export class Quote {
+  showDetails: boolean;
   constructor(
     public id: number,
     public quote: string,
     public author: string,
     public submittedBy: string,
-    public upVote: number,
-    public downVote: number
-  ) {}
+    public upVote: number = 0,
+    public downVote: number = 0
+  ) {
+    this.showDetails = false;
+  }
 }
