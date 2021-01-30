3abfcaf987aac373c18582c268273a7a44c6cfcd
BwanaQ <thunjawax@gmail.com>
2021-01-30 17:29:09 +0300

    ft-logo-returns-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,10 @@ import { FooterComponent } from './components/footer/footer.component';
     QuoteComponent,
     QuoteDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
